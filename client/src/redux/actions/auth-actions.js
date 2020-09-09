f2bc27f87a85c9dd4249287ae296bff580a17ed6
Trey McGarity <trey.mcgarity99@gmail.com>
2020-09-09 16:30:24 -0500

    big initial file structure

---
+++ client/src/redux/actions/auth-actions.js
@@ -0,0 +1,2 @@
+//functions to control axios requests to update state
+import axios from 'axios';
---
+++ client/src/redux/reducers/auth-reducer.js
@@ -0,0 +1,12 @@
+//Handles authentication state from auth-actions.js functions.
+
+const initialState = {
+
+};
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
