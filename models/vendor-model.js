069eee0f6212ef602b5e36708148827f390c68c4
Trey McGarity <trey.mcgarity99@gmail.com>
2021-02-05 19:00:16 -0600

    built vendor model

--- models/vendor-model.js
+++ models/vendor-model.js
@@ -3,18 +3,65 @@ const UserModel = require('./user-model');
 
 class VendorModel extends UserModel {
     /* Owner side of Website:
-    Owners have accounts and add products to Patron side of App.
+    Owners have accounts and add products to Vendor side of App.
     */
 
-    // getVendors() 
+   async getVendors() {
+    try {
+        return await this.getAllUsersByType('vendor');
+    } catch (error) {
+        throw error;
+    }
+} 
 
-    // getVendorByID(id)
+async getVendorByID(id) {
+    try {
+        const Vendor = await getUserById(id, 'vendor')
+        .then(() => {
+            return Vendor
+        })
+    } catch (error) {
+        throw error;
+    }
+}
 
-    // addVendor(data)
+async addVendor(data) {
+    try {
+        await db('vendor').insert({
+            first_name: data.first_name,
+            last_name: data.last_name,
+            email: data.email.toLowerCase(),
+            phone: data.phone,
+            dob: data.dob,
+            password: data.password,
+            gender: data.gender
+        })
+    } catch (error) {
+        throw error;
+    }
+}
 
-    // updateVendor(is, data)
+async updateVendor(id, updatedData) {
+    try{
+        await db('vendor')
+               .where({id: id})
+               .update(updatedData)
+               .then(data => data);
+        return await this.getUserById(id);
+} catch(err){
+        throw err;
+    }
+}
 
-    // deleteVendor(id)
+async deleteVendor(id){
+    try{
+        return await db('vendor')
+                     .where({id})
+                     .del();
+    } catch(err){
+        throw(err);
+    }
+}
 }
 
 module.exports = new VendorModel();
