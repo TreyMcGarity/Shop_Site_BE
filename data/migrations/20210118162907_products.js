bb951a745af7bcf3c0b887f2dc48413fca41ecf8
Trey McGarity <trey.mcgarity99@gmail.com>
2022-05-24 19:35:50 -0500

    update product string lengths

--- data/migrations/20210118162907_products.js
+++ data/migrations/20210118162907_products.js
@@ -2,8 +2,8 @@
 exports.up = async function(knex) {
 	return knex.schema.createTable("product", (tbl) => {
 		tbl.uuid("id").primary().defaultTo(knex.raw("uuid_generate_v4()"));
-		tbl.string("name", 20).notNull();
-		tbl.string("category", 20).notNull();
+		tbl.string("name", 40).notNull();
+		tbl.string("category", 200).notNull();
 		tbl.string("details");
         tbl.float("cost").notNull();
         tbl.integer("stock");
